2a05c2aa25437c0629551c46af59f12a672690ee
antonkalik <antonkalik@gmail.com>
2022-10-13 18:39:16 +0200

    error hook

--- src/hooks/useApi/useApi.spec.js
+++ src/hooks/useApi/useApi.spec.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 import { useApi } from "./index";
 import axios from "axios";
 
@@ -7,28 +7,22 @@ jest.mock("axios");
 axios.get = jest.fn(() => Promise.resolve({ data: [] }));
 
 describe("useApi", () => {
-  // afterEach(() => {
-  //   jest.clearAllMocks();
-  // });
+  it("should return loading", () => {
+    const { result } = renderHook(() => useApi("/testf"));
 
-  // it("should return loading", () => {
-  //   const { result } = renderHook(() => useApi("/testf"));
-  //
-  //   expect(result.current.loading).toBe(true);
-  //   expect(result.current.data).toBe(null);
-  //   expect(result.current.error).toBe(null);
-  // });
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+    expect(result.current.error).toBe(null);
+  });
 
   it("should return data", async () => {
-    const data = [1, 2, 3]
+    const data = [1, 2, 3];
 
     axios.get.mockImplementation(() => {
       return Promise.resolve({ data });
     });
 
-    const { result, waitForNextUpdate, waitForValueToChange } = renderHook(() =>
-      useApi("/test")
-    );
+    const { result, waitForValueToChange } = renderHook(() => useApi("/test"));
 
     await waitForValueToChange(() => result.current.data);
 
@@ -37,7 +31,15 @@ describe("useApi", () => {
     expect(result.current.data).toStrictEqual(data);
   });
 
-  // it("should return error", () => {
-  //   throw new Error("Test not implemented");
-  // });
+  it("should return error", async () => {
+    const error = new Error("test error message");
+    axios.get.mockImplementation(() => {
+      return Promise.reject(error);
+    });
+
+    const { result, waitForValueToChange } = renderHook(() => useApi("/test"));
+    await waitForValueToChange(() => result.current.error);
+
+    expect(result.current.error).toBe(error);
+  });
 });
